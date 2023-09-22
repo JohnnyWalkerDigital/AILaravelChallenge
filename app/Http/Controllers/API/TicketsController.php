611b4bf478deaaa0402fbeee89d1807b58a31d25
Johnny Walker <johnny@johnnywalkerdesign.com>
2023-09-22 18:14:42 +0100

    Add rudimentary error handling to API endpoints

--- app/Http/Controllers/API/TicketsController.php
+++ app/Http/Controllers/API/TicketsController.php
@@ -6,15 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Services\TicketService;
 use App\Ticket;
 use App\User;
+use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Routing\ResponseFactory;
 
 class TicketsController extends Controller
 {
     public function indexUnprocessed(): LengthAwarePaginator
     {
-        $tickets = Ticket::unprocessed()->oldest()->paginate(5);
-
         return Ticket::unprocessed()->oldest()->paginate(5);
     }
 
@@ -23,9 +23,15 @@ class TicketsController extends Controller
         return Ticket::processed()->oldest()->paginate(5);
     }
 
-    public function getTicketsByUser(Request $request): LengthAwarePaginator
+    public function getTicketsByUser(Request $request): LengthAwarePaginator|ResponseFactory
     {
-        $user = User::where('email', $request->email)->firstOrFail();
+        try {
+            $user = User::where('email', $request->email)->firstOrFail();
+        } catch (\Exception $e) {
+            return response('Resource not found', 404)
+                ->header('Content-Type', 'text/plain');
+        }
+
         return Ticket::where('user_id', $user->id)->oldest()->paginate(5);
     }
 
@@ -36,11 +42,17 @@ class TicketsController extends Controller
      */
     public function getStats(): array
     {
+        try {
+            $lastProcessedTicketTime = Ticket::processed()->latest()->pluck('updated_at')->firstOrFail();
+        } catch (\Exception $e) {
+            $lastProcessedTicketTime = null;
+        }
+
         return [
             'totalTickets' => Ticket::count(),
             'totalUnprocessedTickets' => Ticket::unprocessed()->count(),
             'userWithMostTickets' => TicketService::getUserWithMostTickets(),
-            'latestProcessedTicketTime' => Ticket::processed()->latest()->pluck('updated_at')->firstOrFail()
+            'latestProcessedTicketTime' => $lastProcessedTicketTime
         ];
     }
 }
